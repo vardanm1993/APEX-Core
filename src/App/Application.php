6d03b474b7a2f28272506ad9d65927c399e555bf
Vardan <vardanm1993@gmail.com>
2025-07-03 11:09:49 +0400

    Add new features to Application

--- src/App/Application.php
+++ src/App/Application.php
@@ -6,5 +6,46 @@ use Apex\Core\Container\Container;
 
 class Application extends Container
 {
+    protected array $providers = [];
+    protected array $loadedProviders = [];
 
+    public function __construct()
+    {
+
+    }
+
+    public function registerProvider(string $providerClass): void
+    {
+        if (isset($this->loadedProviders[$providerClass])) {
+            return;
+        }
+
+        $provider = new $providerClass($this);
+
+        $provider->register();
+
+        $this->providers[] = $provider;
+        $this->loadedProviders[$providerClass] = true;
+
+        if(method_exists($provider, 'boot')) {
+            $provider->boot();
+        }
+    }
+
+    public function registerConfiguredProviders(array $providers): void
+    {
+        foreach ($providers as $providerClass) {
+            $this->registerProvider($providerClass);
+        }
+    }
+
+    public function getLoadedProviders(): array
+    {
+        return $this->loadedProviders;
+    }
+
+    public function getRegisteredProviders(): array
+    {
+        return $this->providers;
+    }
 }
