7a399002c2b087167b73b8d6ee643586f843f5e0
Vardan <vardanm1993@gmail.com>
2025-07-07 19:50:52 +0400

    Feat: add composer core installer plugin

---
+++ src/Install/CoreInstallerPlugin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Apex\Core\Install;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+
+class CoreInstallerPlugin implements PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ScriptEvents::POST_INSTALL_CMD => 'onPostInstall',
+            ScriptEvents::POST_UPDATE_CMD => 'onPostInstall',
+        ];
+    }
+
+    public static function onPostInstall(Event $event): void
+    {
+        DockerInstaller::copyDockerCompose();
+    }
+}
